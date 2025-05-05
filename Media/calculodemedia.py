dbabacd79d98a2b408d538ed297ecbb61d9a91f6
João Lucas <161651736+joaolucasrebello@users.noreply.github.com>
2025-05-05 15:28:45 -0300

    Update calculodemedia.py

--- Media/calculodemedia.py
+++ Media/calculodemedia.py
@@ -1,11 +1,10 @@
-# Solicita as 4 notas ao usuário
+# Solicita as 3 notas ao usuário
 nota1 = float(input("Digite a primeira nota: "))
 nota2 = float(input("Digite a segunda nota: "))
 nota3 = float(input("Digite a terceira nota: "))
-nota4 = float(input("Digite a quarta nota: "))
 
 # Calcula a média
-media = (nota1 + nota2 + nota3 + nota4) / 4
+media = (nota1 + nota2 + nota3) / 3
 
 # Exibe o resultado
 print(f"A média das notas é: {media:.2f}")
